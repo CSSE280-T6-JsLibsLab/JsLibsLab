66f6cefaa7a19439b5fef56a01457f437f292675
Hanyu Yang <yangh6@rose-hulman.edu>
2021-01-20 07:18:30 +0800

    style for icons + some onclick functions

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,12 +1,3 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
-
-/** namespace. */
 var rhit = rhit || {};
 
 /** globals */
@@ -17,13 +8,27 @@ rhit.functionName = function () {
 	/** function body */
 };
 
-rhit.ClassName = class {
+rhit.PreviewPageController = class {
 	constructor() {
 
+		document.querySelector("#enableListItem").addEventListener("click", (event) => {
+			document.querySelector("#enableListItemIcon").innerHTML =
+			 document.querySelector("#enableListItemIcon").innerHTML == "radio_button_checked" ? "radio_button_unchecked" : "radio_button_checked";
+		})
+
+		document.querySelector("#refreshListItem").addEventListener("click", (event) => {
+			window.location.reload();
+		})
+
+		document.querySelector("#resetListItem").addEventListener("click", (event) => {
+			this.resetElements();
+		})
 	}
 
-	methodName() {
-
+	resetElements() {
+		document.querySelector("#titleText").innerHTML = "Lorem Ipsum Dolor Sit Amet"
+		document.querySelector("#paragraphText1").innerHTML = "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		document.querySelector("#paragraphText2").innerHTML = "<img id=\"sampleImg\" src=\"https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg\" alt=\"sampleImg\">Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare. Morbi tristique senectus et netu set. Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 	}
 }
 
@@ -31,6 +36,7 @@ rhit.ClassName = class {
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	new this.PreviewPageController();
 };
 
 rhit.main();
